0027f0a6593d790a37d11b718aeb2f28a89fe0e2
Saqibbagsiraj <saqibbagsiraj28@gmail.com>
2026-03-14 12:30:22 +0530

    springboot backend project structure is initialized.

---
+++ src/test/java/org/moviesinfo/omdb/OmdbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.moviesinfo.omdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
